aa4b94e749ca3b6601c9c69a37bb257f1c29b37c
Ryan Howard <ryanghoward@protonmail.com>
2022-02-16 16:53:47 -0800

    yeet

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -25,7 +25,7 @@ function find() {
     .groupBy("sc.scheme_id");
 }
 
-function findById(scheme_id) {
+async function findById(scheme_id) {
   // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
@@ -92,6 +92,27 @@ function findById(scheme_id) {
         "steps": []
       }
   */
+  const rows = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme.id", "st.scheme_id")
+    .where("sc.scheme_id", scheme_id)
+    .select("st.*", "sc.scheme_name", "sc.scheme_id")
+    .orderBy("st.step_number");
+
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: [],
+  };
+  rows.forEach((row) => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions,
+      });
+    }
+  });
+  return result;
 }
 
 function findSteps(scheme_id) {
