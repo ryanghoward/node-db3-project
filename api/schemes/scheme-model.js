d8a433d24ad36bde2f6cbd4723618f73240c1302
Ryan Howard <ryanghoward@protonmail.com>
2022-02-16 17:16:54 -0800

    yeet

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -152,6 +152,11 @@ function add(scheme) {
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+  return db("schemes")
+    .insert(scheme)
+    .then(([scheme_id]) => {
+      return db("schemes").where("scheme_id", scheme_id).first();
+    });
 }
 
 function addStep(scheme_id, step) {
